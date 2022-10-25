5d21e07cdf98855249b27b33194b56e296214fe6
Ville <ville-simila@hotmail.com>
2022-10-25 14:51:48 +0300

    valmis

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+const mysql = require('mysql2/promise')
+const config = require('./config')
+const { restart } = require('nodemon')
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: false}))
+
+const port = 3001
+
+app.listen(port)
+
+app.get("/", async function (req, res) {
+    try {
+        const connection = await mysql.createConnection(config.db)
+        const [result,] = await connection.execute('select * from task')
+
+        if (!result) result=[]
+        res.status(200).json(result)
+    } catch(err) {
+        res.status(500).json({error: err.message})
+    }
+})
---
+++ src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const URL = 'http://localhost:3001/'
+
+function App() {
+  const [tasks,setTasks] = useState([])
+
+  useEffect(() => {
+    axios.get(URL)
+      .then((response) => {
+        setTasks(response.data)
+      }).catch (error => {
+        alert(error.response.data.error)
+      })
+  }, [])
+
+  return (
+    <div>
+      <h3>My tasks:</h3>
+      <ol>
+        {tasks.map(task=> (
+          <li key={task.id}>{task.description}</li>
+        ))}
+      </ol>
+    </div>
+  );
+}
+
+export default App;
